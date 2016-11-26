4e0f3da553f21cb59a6b03ab8e09f3c76705ae40
Sven Bendel <ubuntudroid@gmail.com>
2016-11-26 16:05:24 +0100

    Remove redundant delete handling

--- app/src/main/java/com/lovoo/tailor/example/MainActivity.kt
+++ app/src/main/java/com/lovoo/tailor/example/MainActivity.kt
@@ -2,23 +2,11 @@ package com.lovoo.tailor.example
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.lovoo.tailor.ButtonEditText
-import com.lovoo.tailor.CompoundDrawableOnTouchListener
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        delete_button_edit_text.onCompoundDrawableClickListener = object : ButtonEditText.OnCompoundDrawableClickListener {
-            override fun onCompoundDrawableClicked(direction: CompoundDrawableOnTouchListener.Direction) {
-                if (direction == CompoundDrawableOnTouchListener.Direction.DIRECTION_RIGHT) {
-                    // the right compound drawable has been clicked, delete the text
-                    delete_button_edit_text.setText("");
-                }
-            }
-        }
     }
 }
